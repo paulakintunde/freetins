c976ff5569ef05c0a09d37688b856945bfdaee4c
Paul Akin <paul.haking@gmail.com>
2026-08-24 00:40:16 -0700

    Serve canonical routes without redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   trailingSlash: 'always',
   adapter: cloudflare({ imageService: 'compile' }),
   build: {
-    format: 'file',
+    format: 'directory',
   },
   prefetch: {
     defaultStrategy: 'hover',
